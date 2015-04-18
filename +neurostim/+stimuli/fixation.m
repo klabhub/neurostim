3da59fe8fe911faeb11b1d47b66f69a1bf1a694c
bartkrekelberg <bart@xps2013.vision.rutgers.edu>
2015-04-18 00:09:44 -0400

    Fixing the fixation stimulus

--- +neurostim/+stimuli/fixation.m
+++ +neurostim/+stimuli/fixation.m
@@ -5,62 +5,43 @@ classdef fixation < neurostim.stimulus
     methods (Access = public)
         function o = fixation(name)
             o = o@neurostim.stimulus(name);
-            o.addProperty('size',5) ;                                                          
-            o.addProperty('shape','CIRC','',@(x)(ismember(x,{'CIRC','RECT'}))) ;   
-            o.addProperty('angle', 0);  
+            o.addProperty('size',5);
+            o.addProperty('size2',5);
+            o.addProperty('color2',[0 0]);
+            o.addProperty('luminance2',0);
+            o.addProperty('shape','CIRC','',@(x)(ismember(upper(x),{'CIRC','RECT','TRIA','DONUT','OVAL','STAR'}))) ;   
+            o.addProperty('angle', 0);              
+            o.listenToEvent({'BEFOREFRAME'});
+            o.X = 0; 
+            o.Y=  0; 
             o.on = 0;
-            o.listenToEvent({'BEFOREFRAME','AFTERFRAME'});
-            o.X = 0; % X coordinate for top left of screen
-            o.Y=  0; % Y coordinate for top left of screen  
-            o.addProperty('CC',[650,512.5]); %CC= Center Coordinates of o.window
-           
-        end
-        
-        function afterFrame(o,c,evt)
-           % o.X= o.X +100*(rand-0.5);
         end
         
         function beforeFrame(o,c,evt)
-            
-            rct = CenterRect([o.X, o.Y, o.X+o.size(2).*(10), o.Y+o.size(2).*(10)], o.cic.position); %Center a Rectangle on the screen
-            
-            switch upper(o.shape)
-                                   
-                case 'RECT'                
-                    Screen('FillRect', o.cic.window, [o.color o.luminance],rct);
-                    
-                case 'CIRC'
-%                     Screen('glLoadIdentity',o.cic.window);      
-%                     Screen('glPoint',o.cic.window,[o.color o.luminance],o.CC(1),o.CC(2),o.size(2));
-%                     Screen('glLoadIdentity',o.cic.window);
-%                     
-                    Screen('DrawDots', o.cic.window,[o.X o.Y],o.size,[o.color o.luminance],o.cic.center);
-                    
-                case 'CONCIRC'
+            switch upper(o.shape)                                   
+                case 'RECT' % Rectangle                
+                    rct = CenterRect([o.X, o.Y, o.X+o.size, o.Y+o.size], o.cic.position); %Center a Rectangle on the screen
+                    Screen('FillRect', o.cic.window, [o.color o.luminance],rct);                    
+                case 'CIRC' % Circle
+                    Screen('DrawDots', o.cic.window,[o.X o.Y],o.size,[o.color o.luminance],o.cic.center);                    
+                case 'DONUT' % DONUT
+                    Screen('DrawDots', o.cic.window,[o.X o.Y],o.size,[o.color o.luminance],o.cic.center);  
+                    Screen('DrawDots', o.cic.window,[o.X o.Y],o.size2,[o.color2 o.luminance2],o.cic.center);                                
+                case 'TRIA'  %Oriented triangle
                     Screen('glLoadIdentity', o.cic.window);
-                    Screen('glPoint',o.cic.window,[o.color o.luminance],o.CC(1),o.CC(2),o.size(4));
-                    Screen('glPoint',o.cic.window,[1 1 1], o.CC(1), o.CC(2), o.size(2));
-                    Screen('glLoadIdentity', o.cic.window);
-                              
-                case 'TRIA'  %Creates an upward facing triangle, rotated to the right by 90 deg
-                   Screen('glLoadIdentity', o.cic.window);
-                   Screen('glTranslate', o.cic.window, o.cic.position(3)/2, o.cic.position(4)/2);
-                   Screen('glRotate', o.cic.window, o.angle(9), 0, 0, 1) ;   %Make this general
-                  
-                   Screen('glTranslate', o.cic.window, -o.cic.position(3)/2, -o.cic.position(4)/2);
-                   isConvex = 1;
-                   Screen('FillPoly',o.cic.window,[o.color o.luminance],[o.CC(1),o.CC(2)-o.size(3); o.CC(1)-o.size(5),o.CC(2)+o.size(3); o.CC(1)+o.size(5),o.CC(2)+o.size(3);],isConvex);
-                   Screen('glLoadIdentity', o.cic.window);                    
-                 
-                case 'OVAL'                
-                   Screen('FillOval', o.cic.window, [o.color o.luminance], rct);
-                                                             
-                case 'STAR'                 
-                   Screen('FramePoly',o.cic.window,[o.color o.luminance],[o.CC(1)-o.size(5),o.CC(2)+o.size(3); o.CC(1)-o.size(1),o.CC(2)-o.size(6); o.CC(1)+o.size(2),o.CC(2)+o.size(3); o.CC(1)-o.size(6),o.CC(2)-o.size(3); o.CC(1)+o.size(3),o.CC(2)-o.size(3);], [3]);  %This should create a star
-                      
+                    Screen('glTranslate', o.cic.window,o.cic.center(1),o.cic.center(2));                   
+                    % This rotation does not work: (it rotates the o.Y
+                    % too...)
+                    %Screen('glRotate', o.cic.window, o.angle+90, 0, 0, 1) ;  %0.angle = 0 ->points rightward
+                    isConvex = 1;
+                    x = o.X + [-0.5*o.size 0 0.5*o.size];
+                    y = o.Y + [-0.5*o.size +0.5*o.size -0.5*o.size];
+                   Screen('FillPoly',o.cic.window,[o.color o.luminance],[x' y'],isConvex);
+                   Screen('glLoadIdentity', o.cic.window);                                     
+                case 'OVAL'                                  
+                case 'STAR'                                       
                              
             end
-            %Screen('DrawDots', o.cic.window,[o.X o.Y],o.size,[o.color o.luminance],o.cic.center);
         end
         
         
