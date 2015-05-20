a816bb5aed44d238dfe7d1db746d936743cee49b
bartkrekelberg <bart@vision.rutgers.edu>
2015-05-19 22:33:10 -0400

    Improved grating/gabor class with circular masks, but the texture coordinate mapping is off and needs more work.

--- demos/bkDemoExperiment.m
+++ demos/bkDemoExperiment.m
@@ -15,38 +15,33 @@ c.colorMode = 'xyl';
 c.add(plugins.debug);               % Use the debug plugin which allows you to move to the next trial with 'n'
 
 
+gi= stimuli.gabor('inny')
 
-
-t = stimuli.text('text');           % create a text stimulus
-t.message = 'Hello World';
-t.font = 'Times New Roman';
-t.textsize = 6;
-t.textstyle = 'italic';
-t.textalign = 'r';
-t.X = 0;
-t.Y = 0;
-
-% Two variants of functions: using the object t or its name 'text' 
-% This will evalue t.X+5 everytime t.X is requested (and log any changes).
-functional(t,'X',{@plus,{'text','X'},5}); 
-functional(t,'Y',{@plus,{t,'Y'},5});
-
-
-t.color = [255 255 255];
-c.add(t);
-
+c.add(gi);
 % 
-s = stimuli.rdp('dots');
-s.color = [1 1 1];
-s.motionMode = 'spiral';
-s.coherence = 1;
-s.lifetime = inf;
-c.add(s);
+% t = stimuli.text('text');           % create a text stimulus
+% t.message = 'Hello World';
+% t.font = 'Times New Roman';
+% t.textsize = 6;
+% t.textstyle = 'italic';
+% t.textalign = 'r';
+% t.X = 0;
+% t.Y = 0;
+% 
+% % Two variants of functions: using the object t or its name 'text' 
+% % This will evalue t.X+5 everytime t.X is requested (and log any changes).
+% % functional(t,'X',{@plus,{'text','X'},5}); 
+% % functional(t,'Y',{@plus,{t,'Y'},5});
+% 
+% 
+% t.color = [255 255 255];
+% c.add(t);
 
 
 
 
-c.addFactorial('myFactorial',{'dots','motionMode',{0, 1}}) ; %You can add more direction conditions... Key press 'n' changes direction.... e.g: {'right','left',etc..}
+
+c.addFactorial('myFactorial',{'inny','frequency',{0.5, 1}}) ; %You can add more direction conditions... Key press 'n' changes direction.... e.g: {'right','left',etc..}
 c.addBlock('myFactorial',10,'SEQUENTIAL') % Add a block in whcih we run all conditions in the factorial 10 times.
 
 
