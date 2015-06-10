6edd348762be9b8643c2b40e35f2056a210029a3
bartkrekelberg <bart@vision.rutgers.edu>
2015-06-09 21:05:22 -0400

    Cosmetic changes

--- demos/bkDemoExperiment.m
+++ demos/bkDemoExperiment.m
@@ -1,23 +1,33 @@
 
 import neurostim.*
-% Factorweights.
-% Remove KeyStroke?
-commandwindow;
+
 
 Screen('Preference', 'SkipSyncTests', 1);
 Screen('Preference', 'ConserveVRAM', 32);
 
 
 c = cic;                            % Create Command and Intelligence Center...
-c.position = [0 0 200 200];         % Set the position and size of the window
-c.color.background= [0 0 0];
+c.position = [0 0 500 500];         % Set the position and size of the window
+c.color.background= [0.6 0.25 5];
 c.colorMode = 'xyl';
-c.add(plugins.debug);               % Use the debug plugin which allows you to move to the next trial with 'n'
+
+c.add(plugins.gui);                 % Create and add a "GUI" (status report)
+c.add(plugins.debug);               % Use the debug plugin; this allows you to move to the next trial with 'n'
 
 
-gi= stimuli.gabor('inny')
+gi= stimuli.gabor('inny');
+gi.color = [0.5 0.5];
+gi.luminance = 25;
+gi.sigma = 20;
+gi.X = 250;
+gi.Y = 250;
+
+
 
 c.add(gi);
+%qc.add(g);
+
+
 % 
 % t = stimuli.text('text');           % create a text stimulus
 % t.message = 'Hello World';
@@ -41,7 +51,7 @@ c.add(gi);
 
 
 
-c.addFactorial('myFactorial',{'inny','frequency',{0.5, 1}}) ; %You can add more direction conditions... Key press 'n' changes direction.... e.g: {'right','left',etc..}
+c.addFactorial('myFactorial',{'inny','frequency',{0.05, 0.01}}) ; %You can add more direction conditions... Key press 'n' changes direction.... e.g: {'right','left',etc..}
 c.addBlock('myFactorial',10,'SEQUENTIAL') % Add a block in whcih we run all conditions in the factorial 10 times.
 
 
