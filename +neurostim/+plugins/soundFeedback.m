b7373db0b9024f6317974da34bc23ee9f52ca5e0
bartkrekelberg <bart@vision.rutgers.edu>
2016-03-10 12:32:02 +0100

    Simplified soundFeedback a bit by storing waveform nd buffer as members not as autolog properties. In the previous version all waveforms were saved to the output file, which seems excessive.

--- +neurostim/+plugins/soundFeedback.m
+++ +neurostim/+plugins/soundFeedback.m
@@ -1,9 +1,16 @@
 classdef soundFeedback < neurostim.plugins.feedback
     % Plugin to deliver auditory feedback.
-    % 'path'  -  Path to search for sound files that are specified in add()
-    % 'waveform'  - filename for a .wav or other file. Or an actual mono mono- (vector) or stereo (matrix) waveform 
+    % 'path'  -  Path (relative to Neurostim root directory
+    % (cic.dirs.root)) to search for sound files that are specified in
+    % add(). This defaults to 'sounds'.
+    % 'waveform'  - filename for a .wav or other file. Or an actual mono
+    % mono- (vector) or stereo (matrix) waveform. Files will be searched in
+    % the 'path' directory, but full file names (includng path) can also be
+    % given.
     properties (SetAccess=public)
         path@char ='sounds'; % Set this to the folder that contains the sound files. Relative to cic.root
+        buffer  = []; %Vector of buffer handles created by PscyhPortAudio in plugins.sound.m
+        waveform = {}; % Waveforms
     end
     
     
@@ -33,42 +40,40 @@ classdef soundFeedback < neurostim.plugins.feedback
                     file = p.Results.waveform;
                 end
                 if exist(file,'file')
-                    waveform = o.readFile(file);
+                    %Read file and return waveform data        
+                    wave = audioread(file);
                 else
                     o.cic.error('STOPEXPERIMENT',['Sound file ' strrep(file,'\','/') ' could not be found.']);
                 end
             else 
-                waveform = p.Results.waveform; % Presumably the waveform itself
+                wave = p.Results.waveform; % Presumably the waveform itself
             end
             %Store the waveform
-            o.addProperty(['item', num2str(o.nItems) 'waveform'],waveform);
+            o.waveform{o.nItems} = wave;
         end
     end
     
     methods (Access=public)
         
-        function beforeExperiment(o,c,evt)            
+        function beforeExperiment(o,c,~)            
             %Check that the sound plugin is enabled
             snd = c.pluginsByClass('sound');            
             if isempty(snd)
                 o.cic.error('STOPEXPERIMENT','No "sound" plugin detected. soundFeedback relies on it.');
             end            
             %Allocate the audio buffers
+            o.buffer = nan(1,o.nItems);
             for i=1:o.nItems
-               o.addProperty(['item', num2str(i) 'buffer'],o.cic.sound.createBuffer(o.(['item' num2str(i) 'waveform'])));
+               o.buffer(i)  = o.cic.sound.createBuffer(o.waveform{i});
             end
         end
-
-        function waveformData = readFile(o,file)             
-            %Read WAV file and return waveform data
-            waveformData = audioread(file);
-        end
+        
     end
     
     methods (Access=protected)
          
         function deliver(o,item)
-            o.cic.sound.play(o.(['item' num2str(item) 'buffer']));   
+            o.cic.sound.play(o.buffer(item));   
         end
  end
         
