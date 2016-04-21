0750e548d1dd9d8fa42473ec60484add917fbf18
Bart Krekelberg <bart@vision.rutgers.edu>
2016-04-21 22:23:31 +0200

    Using AM versions

--- demos/adamDemo.m
+++ demos/adamDemo.m
@@ -17,7 +17,6 @@ commandwindow;
 
 %% ========= Specify rig configuration  =========
 
-<<<<<<< HEAD
 %Create a CIC object. Here the cic is returned with some default settings intitialised for Adam's rigs.
 c = myRig;
 
@@ -26,11 +25,6 @@ if isempty(c.pluginsByClass('eyetracker'))
     e = neurostim.plugins.eyetracker(c);      %Eye tracker plugin not yet added, so use the virtual one. Mouse is used to control gaze position (click)
     e.useMouse = true;
 end
-=======
-%Create a CIC object. Here the cic is returned with some default settings
-%based on the rig in which the experiment is started.
-c = demoLabConfig;
->>>>>>> improveTiming
 
 %% ============== Add stimuli ==================
 
