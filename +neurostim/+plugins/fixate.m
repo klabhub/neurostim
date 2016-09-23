f31122d679781beeb1abaf5faf3ad06eda82c375
Shaun Cloherty <cnuahs@users.noreply.github.com>
2016-09-23 11:18:08 +1000

    Updated the description of the fixate behaviour plugin.

--- +neurostim/+plugins/fixate.m
+++ +neurostim/+plugins/fixate.m
@@ -1,14 +1,23 @@
 classdef fixate < neurostim.plugins.behavior
-    % Behavioral plugin which monitors fixation on a point.
-    % fixate - behavioural plugin which sets on = true when the fixation
-    % point (X,Y) +/- tolerance has been fixated on for length dur (ms).
+   % Behavioral plugin which monitors fixation on a point.
+   %
+   % A fixate object o behaves as follows:
+   %
+   % In continuous mode (o.continuous = true; the default), o.success
+   % is set to true when eye position comes within o.tolerance of the 
+   % fixation point (o.X,o.Y) within o.from (ms) and remains there
+   % until o.to (ms).
+   %
+   % In discrete mode (o.continuous = false), o.success is set to true
+   % as soon as the eye position comes within o.tolerance of the
+   % fixation point (o.X,o.Y).
    properties (Access=private)
    end
    
    methods
        function o=fixate(c,name)
            o=o@neurostim.plugins.behavior(c,name);
-           o.addProperty('X',0,'validate',@isnumeric);                 %X,Y,Z - the position of a target for the behaviour (e.g. fixation point)
+           o.addProperty('X',0,'validate',@isnumeric); % X,Y,Z - the position of a target for the behaviour (e.g. fixation point)
            o.addProperty('Y',0,'validate',@isnumeric);
            o.addProperty('Z',0,'validate',@isnumeric);
            o.addProperty('tolerance',1,'validate',@isnumeric);
@@ -22,7 +31,8 @@ classdef fixate < neurostim.plugins.behavior
    
    methods (Access=protected)
        function inProgress = validate(o)
-           % validate returns o.on = true when behavior passes all checks.
+           % validate returns true when eye position is within
+           % tolerance of (X,Y).
            inProgress = sqrt((o.cic.eye.x-o.X)^2+(o.cic.eye.y-o.Y)^2)<=o.tolerance;
        end
    end
