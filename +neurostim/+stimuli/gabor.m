bae6d136fdce013b34d16f5be332f79529e4d58e
bartkrekelberg <bart@vision.rutgers.edu>
2017-10-15 12:17:23 -0400

    Moved for-loop for multi gabor orientation masks into the GLSL shader.
    the loop in Matlab was causing excessive framedrops. Inside the shader this works fine.
    This code could easily be adapted to make plaids (curently these multiGabors are essentially random plaids).

--- +neurostim/+stimuli/gabor.m
+++ +neurostim/+stimuli/gabor.m
@@ -9,6 +9,18 @@ classdef gabor < neurostim.stimulus
     % 	sigma - spatial constant of gaussian hull function of gabor
     % 	width, height - maximum size of the gabor.
     % 	mask - one of 'GAUSS','CIRCLE','ANNULUS'
+    %
+    %  This stimulus can also draw the sum of multiple gabors. You can
+    %  choose the number and whether to randomize the phase. 
+    % multiGaborsN  = Must be 10 or less. 
+    % multiGaborsPhaseRand = Boolean to use random phase for each of the
+    % N.(Default is false, which corresponds to zero phase offset between
+    % the different components).
+    % multiGaborsOriRand =  Boolean to use random orientaiton for each of
+    % the N. Default is false, which corresponds to linearly spaced oris
+    % between 0 and 180.
+    %
+
     properties (Constant)
         maskTypes = {'GAUSS','CIRCLE','ANNULUS'};
     end
@@ -41,14 +53,15 @@ classdef gabor < neurostim.stimulus
             o.addProperty('phaseSpeed',0);
             
             %% Special use
-            % Set oriMask to n>1 to create a sum of Gabors masking stimulus
-            % with little if any orientation contents. oriMask =8 will
+            % Set o.multiGaborsN to n>1 to create a sum of Gabors masking stimulus
+            % with little if any orientation contents. o.multiGaborsN =8 will
             % superimpose 8 gabors with random phase, and equally spaced
-            % orientations. oriMask=-8 will randomize the orientations.
-            o.addProperty('oriMask',0,'validate',@isnumeric);
-            o.addProperty('oriMaskPhaseRand',false,'validate',@islogical);
-            o.addProperty('phaseOffset',0); % Used internally to randomize phase for the ori mask
-            o.addProperty('oriOffset',0); % Used internally to determine the orientation of the mask components
+            % orientations. 
+            o.addProperty('multiGaborsN',0,'validate',@isnumeric);
+            o.addProperty('multiGaborsPhaseRand',false,'validate',@islogical);
+            o.addProperty('multiGaborsOriRand',false,'validate',@islogical);         
+            o.addProperty('multiGaborsPhaseOffset',zeros(1,10)); % Used internally to randomize phase for the ori mask
+            o.addProperty('multiGaborsOriOffset',zeros(1,10)); % Used internally to determine the orientation of the mask components
             
         end
         
@@ -63,25 +76,30 @@ classdef gabor < neurostim.stimulus
         
         
         function beforeTrial(o)
-            glUseProgram(o.shader);
-            glUniform1i(glGetUniformLocation(o.shader, 'mask'),find(ismember(o.maskTypes,upper(o.mask))));
-            glUseProgram(0);
-            
-            if o.oriMask~=0
-                n = abs(o.oriMask);  
-                if o.oriMaskPhaseRand 
-                    o.phaseOffset = 360*rand(1,n); % 
-                else
-                    o.phaseOffset = zeros(1,n);    
+            if o.multiGaborsN~=0              
+                if o.multiGaborsN>10
+                    error('Max 10 gabors in a multi gabor');
                 end
-                if o.oriMask<0
-                    o.oriOffset= 180*rand(1,n); % Random orientations for n<0
+                if o.multiGaborsPhaseRand 
+                    o.multiGaborsPhaseOffset = 360*rand(1,o.multiGaborsN); % 
                 else
-                    o.oriOffset= linspace(0,180,n);
+                    o.multiGaborsPhaseOffset = zeros(1,o.multiGaborsN);    
                 end
+                if o.multiGaborsOriRand
+                    o.multiGaborsOriOffset= 180*rand(1,o.multiGaborsN); % Random orientations for n<0
+                else
+                    o.multiGaborsOriOffset= linspace(0,180,o.multiGaborsN);
+                end                
             end
             
-            
+            % Pass information that does not change during the trial to the
+            % shader.
+            glUseProgram(o.shader);
+            glUniform1i(glGetUniformLocation(o.shader, 'mask'),find(ismember(o.maskTypes,upper(o.mask))));
+            glUniform1i(glGetUniformLocation(o.shader, 'multiGaborsN'),max(1,o.multiGaborsN)); % At least 1 so that a single Gabor is drawn
+            glUniform1fv(glGetUniformLocation(o.shader, 'multiGaborsPhaseOffset'),numel(o.multiGaborsPhaseOffset),o.multiGaborsPhaseOffset);
+            glUniform1fv(glGetUniformLocation(o.shader, 'multiGaborsOriOffset'),numel(o.multiGaborsOriOffset),o.multiGaborsOriOffset);
+            glUseProgram(0);                        
             
         end
         
@@ -100,20 +118,10 @@ classdef gabor < neurostim.stimulus
                 oColor = [oColor 0 0];% Luminance only spec (probably M16 mode)
             end
             
-                       
-            if o.oriMask~=0
-                % Adjust alpha to overlay Gabors in the oriMask
-                n =  abs(o.oriMask);
-                o.alpha = 1/n;                
-            else
-                n=1;
-            end
-            
-            % Draw the Gabor using the GLSL shader
-            for i=1:n
-                aux = [(o.phase+o.phaseOffset(i)), o.frequency, oSigma; o.contrast 0 0 0]';    
-                Screen('DrawTexture', o.window, o.texture, sourceRect, o.textureRect, o.orientation+o.oriOffset(i), filterMode, globalAlpha, [oColor, o.alpha] , textureShader,specialFlags, aux);
-            end
+                                            
+            % Draw the Gabor using the GLSL shader            
+            aux = [o.phase, o.frequency, oSigma; o.contrast 0 0 0]';    
+            Screen('DrawTexture', o.window, o.texture, sourceRect, o.textureRect, o.orientation, filterMode, globalAlpha, [oColor, o.alpha] , textureShader,specialFlags, aux);            
 
         end
         
