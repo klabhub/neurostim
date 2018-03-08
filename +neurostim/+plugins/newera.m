bdcff8ebbfc89ff46238a22b8ee7e18927eefa46
Shaun Cloherty <s.cloherty@ieee.org>
2018-03-09 10:10:10 +1100

    wip: newera plugin working, no support for mcc yet

--- +neurostim/+plugins/newera.m
+++ +neurostim/+plugins/newera.m
@@ -5,6 +5,14 @@
 classdef newera <  neurostim.plugins.feedback
   % Wrapper class for New Era syringe pumps (see http://syringepump.com/).
   %
+  
+  % usage:
+  %
+  %   r = plugins.newera();
+  %   r.add('volume',100,'when','afterFrame','criterion','@fixation1.success');
+  %   r.add('volume',500,'when','afterTrial','criterion','@fixation2.success');
+  %
+  
   % To see the public properties of this class, type
   %
   %   properties(neurostim.plugins.newera)
@@ -27,94 +35,98 @@ classdef newera <  neurostim.plugins.feedback
   properties (SetAccess = private, GetAccess = public)
     dev@serial; % the serial port object - PRIVATE?
 
-    port; % port for serial communications ('COM1','COM2', etc.)
-    baud;
+%     port; % port for serial communications ('COM1','COM2', etc.)
+%     baud;
+%     
+%     address@double; % pump address (0-99)
     
-    address@double; % pump address (0-99)
+    mcc; % (boolean) set to TRUE if the mcc is available
     
-    mcc; % (optional) we'll use the MCC if available
+    % fot the gui...
+    nrDelivered = 0;
+    totalDelivered = 0;
   end % properties
 
   % dependent properties, calculated on the fly...
-  properties (Dependent, SetAccess = public, GetAccess = public)
-    diameter@double; % diameter of the syringe (mm)
-    volume@double;   % dispensing volume (mL)
-    rate@double;     % dispensing rate (mL per minute)
-  end
-
-  methods % set/get dependent properties
-    % dependent property set methods
-    function o = set.diameter(o,value)
-        o.setdia(value);
-    end
-
-    function o = set.volume(o,value)
-        % note: value is in ml, however, if diameter > 14.0mm, the pump
-        %       is expecting volume in microliters (unless the default units
-        %       have been over-riden).
-        if o.diameter <= 14.0,
-          value = value*1e3; % microliters
-        end
-        o.setvol(value);
-    end
-
-    function o = set.rate(o,value)
-        o.setrate(value);
-    end
-
-    % dependent property get methods
-    function value = get.diameter(o)
-        [err,status,msg] = o.sndcmd('DIA');
-        assert(err == 0);
-
-        value = str2num(msg);
-    end
-
-    function value = get.volume(o)
-      [err,status,msg] = o.sndcmd('VOL');
-      assert(err == 0);
-
-      pat = '(?<value>[\d\.]{5})\s*(?<units>[A-Z]{2})';
-      tokens = regexp(msg,pat,'names');
-        
-      value = str2num(tokens.value);        
-
-      % note: value should be returned in ml, however, if diameter <= 14.0mm,
-      %       the pump returns the volume in microliters (unless the default
-      %       units have been over-riden).
-      switch upper(tokens.units),
-        case 'ML', % milliliters
-          value = value;
-        case 'UL', % microliters
-          value = value/1e3; % milliliters
-        otherwise,
-          warning('NEUROSTIM:NEWERA','Unknown volume units ''%s''.', tokens.units);
-      end
-    end
-
-    function value = get.rate(o)
-      [err,status,msg] = o.sndcmd('RAT');
-      assert(err == 0);
-
-      pat = '(?<value>[\d\.]{5})\s*(?<units>[A-Z]{2})';
-      tokens = regexp(msg,pat,'names');
-        
-      value = str2num(tokens.value);
-      
-      switch upper(tokens.units),
-        case 'MM', % milliliters per minute
-          value = value;
-        case 'MH', % millimeters per hour
-          value = value/60.0; % milliliters per minute
-        case 'UM', % microliters per minute
-          value = value/1e3; % milliliters per minute
-        case 'UH', % microliters per hour
-          value = value/(60*1e3); % milliliters per minute
-        otherwise,
-          warning('NEUROSTIM:NEWERA','Unknown rate units ''%s''.', tokens.units);
-      end 
-    end
-  end
+%   properties (Dependent, SetAccess = public, GetAccess = public)
+%     diameter@double; % diameter of the syringe (mm)
+%     volume@double;   % dispensing volume (mL)
+%     rate@double;     % dispensing rate (mL per minute)
+%   end
+% 
+%   methods % set/get dependent properties
+%     % dependent property set methods
+%     function o = set.diameter(o,value)
+%         o.setdia(value);
+%     end
+% 
+%     function o = set.volume(o,value)
+%         % note: value is in ml, however, if diameter > 14.0mm, the pump
+%         %       is expecting volume in microliters (unless the default units
+%         %       have been over-riden).
+%         if o.diameter <= 14.0,
+%           value = value*1e3; % microliters
+%         end
+%         o.setvol(value);
+%     end
+% 
+%     function o = set.rate(o,value)
+%         o.setrate(value);
+%     end
+% 
+%     % dependent property get methods
+%     function value = get.diameter(o)
+%         [err,status,msg] = o.sndcmd('DIA');
+%         assert(err == 0);
+% 
+%         value = str2num(msg);
+%     end
+% 
+%     function value = get.volume(o)
+%       [err,status,msg] = o.sndcmd('VOL');
+%       assert(err == 0);
+% 
+%       pat = '(?<value>[\d\.]{5})\s*(?<units>[A-Z]{2})';
+%       tokens = regexp(msg,pat,'names');
+%         
+%       value = str2num(tokens.value);        
+% 
+%       % note: value should be returned in ml, however, if diameter <= 14.0mm,
+%       %       the pump returns the volume in microliters (unless the default
+%       %       units have been over-riden).
+%       switch upper(tokens.units),
+%         case 'ML', % milliliters
+%           value = value;
+%         case 'UL', % microliters
+%           value = value/1e3; % milliliters
+%         otherwise,
+%           warning('NEUROSTIM:NEWERA','Unknown volume units ''%s''.', tokens.units);
+%       end
+%     end
+% 
+%     function value = get.rate(o)
+%       [err,status,msg] = o.sndcmd('RAT');
+%       assert(err == 0);
+% 
+%       pat = '(?<value>[\d\.]{5})\s*(?<units>[A-Z]{2})';
+%       tokens = regexp(msg,pat,'names');
+%         
+%       value = str2num(tokens.value);
+%       
+%       switch upper(tokens.units),
+%         case 'MM', % milliliters per minute
+%           value = value;
+%         case 'MH', % millimeters per hour
+%           value = value/60.0; % milliliters per minute
+%         case 'UM', % microliters per minute
+%           value = value/1e3; % milliliters per minute
+%         case 'UH', % microliters per hour
+%           value = value/(60*1e3); % milliliters per minute
+%         otherwise,
+%           warning('NEUROSTIM:NEWERA','Unknown rate units ''%s''.', tokens.units);
+%       end 
+%     end
+%   end
 
   methods
     function o = newera(c,varargin) % c is the neurostim cic
@@ -122,42 +134,82 @@ classdef newera <  neurostim.plugins.feedback
 
       o = o@neurostim.plugins.feedback(c,'newera'); % call parent constructor
      
-      % initialise input parser
-      args = varargin;
-      p = inputParser;
-      p.KeepUnmatched = true;
-      p.StructExpand = true;
-      p.addParamValue('port','COM1',@ischar); % default to COM1?
-      p.addParamValue('baud',19200,@(x) any(ismember(x,[300, 1200, 2400, 9600, 19200])));
+%       % initialise input parser
+%       args = varargin;
+%       p = inputParser;
+%       p.KeepUnmatched = true;
+%       p.StructExpand = true;
+%       p.addParamValue('port','COM1',@ischar); % default to COM1?
+%       p.addParamValue('baud',19200,@(x) any(ismember(x,[300, 1200, 2400, 9600, 19200])));
+% 
+%       p.addParamValue('address',0,@(x) isreal);
+%       
+%       p.addParamValue('diameter',20.0,@isreal); % mm
+% %      p.addParamValue('volume',0.010,@isreal); % ml
+%       p.addParamValue('rate',10.0,@isreal); % ml per minute
+% 
+%       p.parse(varargin{:});
+% 
+%       args = p.Results;
+% 
+%       o.port = args.port;
+%       o.baud = args.baud;
+% 
+%       o.address = args.address;
 
-      p.addParamValue('address',0,@(x) isreal);
-      
-      p.addParamValue('diameter',20.0,@isreal); % mm
-%      p.addParamValue('volume',0.010,@isreal); % ml
-      p.addParamValue('rate',10.0,@isreal); % ml per minute
+      o.addProperty('port','COM1','validate',@ischar); % or something like '/dev/ttyUSB0' on linux
+      o.addProperty('baud',19200,'validate',@(x) any(ismember(x,[300, 1200, 2400, 9600, 19200])));
+      o.addProperty('address',0,'validate',@isreal);
+       
+      o.addProperty('diameter',20.0,'validate',@isreal); % mm
+%      o.addProperty('volume',0.010,'validate',@isreal); % ml
+      o.addProperty('rate',10.0,'validate',@isreal); % ml per minute
 
-      p.parse(varargin{:});
-
-      args = p.Results;
-
-      o.port = args.port;
-      o.baud = args.baud;
-
-      o.address = args.address;
-
-      o.addProperty('mccChannel',9);
+      o.addProperty('mccChannel',9,'validate',@isreal);
       
       % now try and connect to the New Era syringe pump...
       %
       %   data frame: 8N1 (8 data bits, no parity, 1 stop bit)
       %   terminator: CR (0x0D)
-      o.dev = serial(o.port,'BaudRate',o.baud,'DataBits',8,'Parity','none', ...
-                            'StopBits',1,'Terminator',13,'InputBufferSize',4096); % CR = 13
+%       o.dev = serial(o.port,'BaudRate',o.baud,'DataBits',8,'Parity','none', ...
+%                             'StopBits',1,'Terminator',13,'InputBufferSize',4096); % CR = 13
+% 
+%       try
+%         [err,status] = o.open();
+%       catch
+%         error('NEUROSTIM:NEWERA','Could not connect to New Era syringe pump on %s!',o.port);
+%       end
+% 
+%       % configure the pump...
+%       if status ~= 0, % 0 = stopped
+%         o.stop();
+%       end
+% 
+% %       o.diameter = args.diameter;
+% %       o.volume = args.volume;
+% %       o.rate = args.rate;
+%        o.setdia(o.diameter);
+% %        o.setvol(o.volume);
+%        o.setrate(o.rate);
+% 
+%       o.setdir(0); % 0 = infusion, 1 = withdrawal
+%       o.clrvol(0); % 0 = infused volume, 1 = withdrawn volume
+% %       o.clrvol(1);
+    end
 
+    function beforeExperiment(o)
+      % try and connect to the New Era syringe pump...
+      %
+      %   data frame: 8N1 (8 data bits, no parity, 1 stop bit)
+      %   terminator: CR (0x0D)
+      o.dev = serial(o.port,'BaudRate',o.baud,'DataBits',8,'Parity','none', ...
+                     'StopBits',1,'Terminator',13,'InputBufferSize',4096); % CR = 13
+
+      % check that the pump is present,
       try
         [err,status] = o.open();
       catch
-        error('NEUROSTIM:NEWERA','Could not connect to New Era syringe pump on %s!',o.port);
+        o.cic.error('CONTINUE','Liquid reward added but the NewEra pump isn''t responding.');
       end
 
       % configure the pump...
@@ -165,31 +217,25 @@ classdef newera <  neurostim.plugins.feedback
         o.stop();
       end
 
-      o.diameter = args.diameter;
-      o.volume = args.volume;
-      o.rate = args.rate;
-      
+% %       o.diameter = args.diameter;
+% %       o.volume = args.volume;
+% %       o.rate = args.rate;
+      o.setdia(o.diameter);
+%       o.setvol(o.volume);
+      o.setrate(o.rate);
+
       o.setdir(0); % 0 = infusion, 1 = withdrawal
       o.clrvol(0); % 0 = infused volume, 1 = withdrawn volume
 %       o.clrvol(1);
-    end
-
-    function beforeExperiment(o)
-        % check that the pump is present, 
-        try
-            o.open();
-        catch
-            o.cic.error('CONTINUE','Liquid reward added but the NewEra plugin isn''t responding.');
-        end
         
-         % look for the MCC plugin...
-         o.mcc = pluginsByClass(o.cic,'mcc');
-         if numel(o.mcc)==1
-             %Iniatilise the bit low
-             o.mcc.digitalOut(o.mccChannel,false);
-         else
-             o.cic.error('CONTINUE','Liquid reward added but no MCC plugin added (or, more than one added - currently not supported)');
-         end
+%         % look for the MCC plugin...
+%         o.mcc = pluginsByClass(o.cic,'mcc');
+%         if numel(o.mcc)==1
+%             %Iniatilise the bit low
+%             o.mcc.digitalOut(o.mccChannel,false);
+%         else
+%             o.cic.error('CONTINUE','Liquid reward added but no MCC plugin added (or, more than one added - currently not supported)');
+%         end
     end
        
     function afterExperiment(o)
@@ -203,12 +249,20 @@ classdef newera <  neurostim.plugins.feedback
     function chAdd(o,varargin)
        p = inputParser;
        p.StructExpand = true; % The parent class passes as a struct
-       p.parse(varargin{:});      
+       p.addParamValue('volume',0.0,@isreal); % ml
+       p.parse(varargin{:});
+       
+       args = p.Results;
+       
+       o.addProperty(['item', num2str(o.nItems), 'volume'],args.volume);
     end
           
     function deliver(o,item)
 %       fprintf(1,'neurostim.plugins.newera.deliver()\n');
 
+      volume = o.(['item', num2str(item) 'volume']);
+      o.setvol(volume);
+      
       % too slow, this calls the sndcmd() method which involves both a
       % synchronous write operation *and* a synchronous read operation
 %       err = run(o);
@@ -222,7 +276,9 @@ classdef newera <  neurostim.plugins.feedback
       fprintf(o.dev,'00 RUN');
       
       o.nrDelivered = o.nrDelivered + 1;
-      o.totalDelivered = o.totalDelivered + o.volume; % ml
+      o.totalDelivered = o.totalDelivered + volume; % ml
+      
+      % TODO: user the MCC if we have it... should be faster
     end
     
     function report(o)
