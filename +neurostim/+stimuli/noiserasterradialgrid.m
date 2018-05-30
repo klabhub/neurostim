e66710efc831a956f35d01503cfb944fedb009f7
Adam Morris <adam.p.morris@outlook.com>
2018-05-30 20:04:57 +1000

    Added a messy, horrible version of the radial grid (see whitenoiseRadialDemo.m), which demonstrates the general use of the abstract class to a non-Cartesian grid format.

--- +neurostim/+stimuli/noiserasterradialgrid.m
+++ +neurostim/+stimuli/noiserasterradialgrid.m
@@ -10,15 +10,17 @@ classdef noiserasterradialgrid < neurostim.stimuli.noiserasterclut
             
             %User-definable
             o.addProperty('nSectors',8,'validate',@(x) isnumeric(x)); 
-            o.addProperty('nRadii',4,'validate',@(x) isnumeric(x)); 
+            o.addProperty('nRadii',4,'validate',@(x) isnumeric(x));
+            
+            o.writeToFeed('WARNING: The radial noise stimulus is just testing a concept. Horrible. Do not use');
         end
 
         function beforeTrial(o)
             
             %The image is specified as a bitmap in which each pixel value is
             %the ID of the random (luminance) variable to be used.
-            imInds = 1:o.nSectors*o.nRadii;
-            o.idImage = reshape(imInds,o.nSectors,o.nRadii);
+%             imInds = 1:o.nSectors*o.nRadii;
+%             o.idImage = reshape(imInds,o.nSectors,o.nRadii);
             
             %*******
             % Set up a shader (maybe?) to wrap the rectangular idImage
@@ -26,6 +28,28 @@ classdef noiserasterradialgrid < neurostim.stimuli.noiserasterclut
             % create a polar grid?
             %Like this? https://www.shadertoy.com/view/XdBSzz
             %***********
+            nPixels = o.cic.screen.ypixels;
+            x=linspace(-1,1,nPixels);
+            [xGrid,yGrid]=meshgrid(x,x);
+            [pixTh,pixR]=cart2pol(xGrid,yGrid);
+            
+            sectBinWidth = 2*pi/o.nSectors;
+            sectBins = linspace(sectBinWidth/2,2*pi-sectBinWidth/2,o.nSectors);
+            radBinWidth = 1./o.nRadii;
+            radBins = linspace(radBinWidth/2,1-radBinWidth/2,o.nRadii);
+            
+            im=zeros(size(xGrid));
+            ind=1;
+            for i=1:o.nSectors
+                for j=1:o.nRadii
+                    inBin = abs(angle(exp(1i*pixTh)./exp(1i*sectBins(i)))) <= sectBinWidth/2;
+                    inBin = inBin & abs(pixR-radBins(j)) <= radBinWidth/2;
+                    im(inBin) = ind;
+                    ind=ind+1;
+                end
+            end
+
+            o.idImage = im;
             
             %Set up the CLUT and random variable callback functions
             initialise(o);
---
+++ demos/whitenoiseRadialDemo.m
@@ -0,0 +1,42 @@
+function c=whitenoiseRadialDemo
+%   This demo shows how to present a grid of pixel noise, for reverse
+%   correlation analysis and/or signal-in-noise detection tasks.
+
+import neurostim.*
+commandwindow;
+
+%% ========= Specify rig configuration  =========
+
+%Create a Command and Intelligence Centre object (the central controller for everything). Here a cic is returned with some default settings for this computer, if it is recognized.
+c = myRig;
+
+%% ============== Add stimuli ==================
+wn = stimuli.noiserasterradialgrid(c,'grid');
+%wn.size = [32,64];          %Dimensionality of raster (30 texels high, 50 wide)
+wn.height = 13;   %Width and height on screen
+wn.width = 13;
+wn.distribution = 'normal'; %Distribution from which luminance values are drawn
+wn.parms = {128 40};          %{mean sd}
+signalContrast = 0.15;
+wn.bounds = (1-signalContrast)*[-128,128];%Truncate the distribution.
+wn.bounds = [0 255];
+%Specify a signal to embed (the embedding happens automatically in the stimulus class)
+%sig=sin(linspace(0,8*pi,wn.size(2)));
+%wn.signal = signalContrast*repmat(sig,wn.size(1),1)*127+127;
+
+%% Experimental design
+c.trialDuration = 30000; 
+
+%Specify experimental conditions
+myDesign=design('myFac');                      %Type "help neurostim/design" for more options.
+myDesign.fac1.grid.X = [-10 0 10];  
+
+%Specify a block of trials
+myBlock=block('myBlock',myDesign);             %Create a block of trials using the factorial. Type "help neurostim/block" for more options.
+myBlock.nrRepeats=10;
+
+%% Run the experiment.
+c.order('grid');   %Ignore this for now - we hope to remove the need for this.
+c.subject = 'easyD';
+c.run(myBlock);
+
