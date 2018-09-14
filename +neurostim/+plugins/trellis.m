6af68131827acfe39912ee837272013039b079e0
bartkrekelberg <bart@vision.rutgers.edu>
2018-09-14 15:35:41 +0200

    allow reading first set of test files. Temporary\

---
+++ +neurostim/+plugins/trellis.m
@@ -0,0 +1,10 @@
+classdef trellis < neurostim.plugins.ripple
+
+    methods
+
+        function o = trellis(c,name)
+            
+            o = o@neurostim.plugins.ripple(c,name);
+        end
+    end
+end
