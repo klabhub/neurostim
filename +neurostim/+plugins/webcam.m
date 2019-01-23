4df315cdab66dc78c27861b2dfe5a3b859a05cf9
duijnhouwer <j.duijnhouwer@gmail.com>
2019-01-23 18:17:17 -0500

    webcam work in progress

--- +neurostim/+plugins/webcam.m
+++ +neurostim/+plugins/webcam.m
@@ -3,31 +3,42 @@ classdef webcam < neurostim.plugin
     % Jacob 2019-01-21
     
     properties (Access=private)
-        cam@webcam=[];
-        zoomboxidx@logical=[]; % index of zoom box (subset of frame that's stored
-        zoomboxwid@double=[];
-        zoomboxhei@double=[];
+        cam=[];
+        zoomboxidx=[]; % index of zoom box (subset of frame that's stored
+        zoomboxwid=[];
+        zoomboxhei=[];
     end
-    properties % (Access=hidden)
-        frame@uint8=0; % Should have default values, otherwise behavior checking can fail.
-        timestamp@double=NaN; % timestamp provided by "webcam.snapshot", store comparison with internal timestampe
-    end
+  %  properties % (Access=hidden)
+  %      frame@uint8=uint8(0); % Should have default values, otherwise behavior checking can fail.
+  %      timestamp@double=NaN; % timestamp provided by "webcam.snapshot", store comparison with internal timestampe
+  %  end
     methods
-        function o = webcam(c)
-            o = o@neurostim.plugin(c,'webcam');
-            o.addProperty('name','');
+        function o=webcam(c)
+            o=o@neurostim.plugin(c,'webcam');
+            o.addProperty('id',''); % e.g. 'Logitech HD Webcam C615'
             o.addProperty('res','');
-            o.addProperty('zoombox',''); % leftx topy width height
+            o.addProperty('zoombox',[]); % leftx topy width height
             o.addProperty('opt',struct); % struct with additional options such as 'Brightness' or 'WhiteBalanceMode' which are nor available on all webcam
             o.addProperty('gray',false);
+            o.addProperty('snaphz',20);
+            o.addProperty('snaps',uint8(0));
+            o.addProperty('timestamps',[]);
         end
         function beforeExperiment(o)
             connectedCams=webcamlist;
             if isempty(connectedCams)
                 o.cic.error('STOPEXPERIMENT','No webcam found');
             end
-            if isempty(o.name)
-                o.cam = webcam(connectedCams{1});
+            try
+                if isempty(o.id)
+                    o.cam=webcam(connectedCams{1});
+                else
+                    try
+                        o.cam=webcam(o.id);
+                    catch me
+                        o.cic.error('STOPEXPERIMENT',me.message);
+                    end
+                end
             end
             if isempty(o.res)
                 o.res=o.cam.Resolution; % store the default resolution of the connected cam in the resolution property
@@ -53,17 +64,28 @@ classdef webcam < neurostim.plugin
                 if all(o.zoomboxidx)
                     o.zoomboxidx=[]; % no need to zoom if entire frame is inside box
                 end
-            end     
+            end
         end
         function afterFrame(o)
-            [fr, o.timestamp] = snapshot(o.cam);
-            if ~isempty(o.zoomboxidx)
-                fr=reshape(fr(o.zoomboxidx),o.zoomboxhei,o.zoomboxwid,3);
+            % Getting single snaphot from the webcam takes about half a ms.
+            % But ask them in rapid succession and it takes much longer.
+            % Presumably it gives the same snapshot only once, and then
+            % when asked for a snapshot again we have to wait for the
+            % webcam to make the next frame. Therefore, only ask for
+            % snapshot at a rate that's below the framerate of the
+            % camera.
+            persistent lastsnap
+            if isempty(lastsnap) || GetSecs-lastsnap>1/o.snaphz
+                [ss, o.timestamps]=o.cam.snapshot;
+                lastsnap=GetSecs;
+                if ~isempty(o.zoomboxidx)
+                    ss=reshape(ss(o.zoomboxidx),o.zoomboxhei,o.zoomboxwid,3);
+                end
+                if o.gray
+                    ss=rgb2gray(ss);
+                end
+                o.snaps=ss;
             end
-            if o.gray
-                fr=uint8(round(mean(fr,3)));
-            end
-            o.frame=fr;
         end
         function afterExperiment(o)
             o.cam.delete;
