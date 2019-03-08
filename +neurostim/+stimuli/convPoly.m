b279020f209a7999f69ede4301782b2c874254f9
Adam Morris <adam.p.morris@outlook.com>
2019-03-08 12:28:45 +1100

    convPoly: Added ability to specify vertices directly. Also reversed a recent addition (by @ktechvision) which added an option for sinusoidal modulation. This is too specific and arbitrary to include in this stimulus. If this is needed for a particular experiment, it could be achieved using either a derived class (e.g. convPolyFlicker) or a ns function (e.g. o.color = '@sin(....)')

--- +neurostim/+stimuli/convPoly.m
+++ +neurostim/+stimuli/convPoly.m
@@ -1,20 +1,15 @@
 classdef convPoly < neurostim.stimulus
     % Draws an equilateral convex polygon with variable sides.
     % Equilateral convex polygon (e.g. triangle, square, pentagon, hexagon
-    %etc.). Can also create a circle if "nSides" is set to a large number.
+    % etc.). Can also create a circle if "nSides" is set to a large number.
+    % Alternatively, can supply a set of arbitrary vertices to pass to
+    % PTB's FillPoly/FramePoly
     %
     % Adjustable variables:
     %   radius - in physical size.
     %   nSides - number of sides.
     %   filled - true or false.
     %   linewidth - only for unfilled polygon, in pixels.
-    %
-    % The 'color' can be modulated sinusoidally using the following
-    % parameters:  color =  o.color*(1+amplitude*sind(phase+360*time*frequency/1000)
-    %  frequency = sinusoidal flicker frequency in Hz  [0].
-    %  phase - phase of the flicker in degrees. [0]
-    % amplitude - amplitude of the flicker.  [Default is 0: no flicker]
-    %
     properties
     end
     
@@ -25,28 +20,23 @@ classdef convPoly < neurostim.stimulus
             o.addProperty('nSides',5,'validate',@isnumeric);
             o.addProperty('filled',true,'validate',@islogical);
             o.addProperty('linewidth',10,'validate',@isnumeric); %Used only for unfilled polygon.
-            o.addProperty('frequency',0,'validate',@isnumeric); % Hz
-            o.addProperty('phase',0,'validate',@isnumeric); % degrees
-            o.addProperty('amplitude',0,'validate',@isnumeric); %o.color is the mean, 
+            o.addProperty('vx',0,'validate',@isnumeric);        %
+            o.addProperty('vy',0,'validate',@isnumeric);
         end
         
         function beforeFrame(o)
-            %Compute vertices
-            th = linspace(0,2*pi,o.nSides+1);
-            [vx,vy] = pol2cart(th,o.radius);
             
-            if o.amplitude>0
-                % Use sinusoidal flicker
-                thisColor  = o.color * (1+o.amplitude*sind(o.phase + 360*o.time*(o.frequency/1000)));
-            else
-                thisColor = o.color;
+            if isempty(o.vx) 
+                %Compute vertices
+                th = linspace(0,2*pi,o.nSides+1);
+                [o.vx,o.vy] = pol2cart(th,o.radius);
             end
-               
+              
             %Draw
             if o.filled
-                Screen('FillPoly',o.window, thisColor,[vx(:),vy(:)],1);
+                Screen('FillPoly',o.window, o.color,[o.vx(:),o.vy(:)],1);
             else
-                Screen('FramePoly',o.window, thisColor,[vx(:),vy(:)],o.linewidth);
+                Screen('FramePoly',o.window, o.color,[o.vx(:),o.vy(:)],o.linewidth);
             end
         end
     end
