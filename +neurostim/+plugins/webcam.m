eaf01e2d8943b40dcb4dfa64b70afc40b7965757
duijnhouwer <j.duijnhouwer@gmail.com>
2019-03-15 17:31:49 -0400

    added first version of webcam.m

--- +neurostim/+plugins/webcam.m
+++ +neurostim/+plugins/webcam.m
@@ -1,94 +1,28 @@
 classdef webcam < neurostim.plugin
-    % Webcam plugin
-    % Jacob 2019-01-21
     
-    properties (Access=private)
-        cam=[];
-        zoomboxidx=[]; % index of zoom box (subset of frame that's stored
-        zoomboxwid=[];
-        zoomboxhei=[];
+    % Plugin to control udpcam to record video with a webcam during an
+    % experiment and save the results to file. udpcam should be run in a
+    % separate matlab session (start one using !matlab&) or packaged as a
+    % windows executable. an earlier effort grabbed frames from the webcam
+    % within neurostim, in afterframe. but that doesn't work because
+    % grabbing a frame from the webcam locks up execution until the webcam
+    % has a new frame ready. Because webcams typically run at 15-30Hz, this
+    % caused massive framedrops. updcam and it's companian class
+    % updcam_remote_control (around which this plugin is merely a wrapper)
+    can be found at https://github.com/duijnhouwer/udpcam
+    properties (Access=public)
+        RC@udpcam_remote_control;
     end
-  %  properties % (Access=hidden)
-  %      frame@uint8=uint8(0); % Should have default values, otherwise behavior checking can fail.
-  %      timestamp@double=NaN; % timestamp provided by "webcam.snapshot", store comparison with internal timestampe
-  %  end
-    methods
-        function o=webcam(c)
-            o=o@neurostim.plugin(c,'webcam');
-            o.addProperty('id',''); % e.g. 'Logitech HD Webcam C615'
-            o.addProperty('res','');
-            o.addProperty('zoombox',[]); % leftx topy width height
-            o.addProperty('opt',struct); % struct with additional options such as 'Brightness' or 'WhiteBalanceMode' which are nor available on all webcam
-            o.addProperty('gray',false);
-            o.addProperty('snaphz',20);
-            o.addProperty('snaps',uint8(0));
-            o.addProperty('timestamps',[]);
-        end
-        function beforeExperiment(o)
-            connectedCams=webcamlist;
-            if isempty(connectedCams)
-                o.cic.error('STOPEXPERIMENT','No webcam found');
+    methods (Access=public)
+        function o=webcam(c,name)
+            if nargin==1
+                name=mfilename;
             end
-            try
-                if isempty(o.id)
-                    o.cam=webcam(connectedCams{1});
-                else
-                    try
-                        o.cam=webcam(o.id);
-                    catch me
-                        o.cic.error('STOPEXPERIMENT',me.message);
-                    end
-                end
-            end
-            if isempty(o.res)
-                o.res=o.cam.Resolution; % store the default resolution of the connected cam in the resolution property
-            else
-                try
-                    o.cam.Resolution=o.res; % will fail if o.res is not in o.cam.Resolution
-                catch me
-                    o.cic.error('STOPEXPERIMENT',me.message);
-                end
-            end
-            if ~isempty(o.zoombox)
-                R=regexp(o.cam.Resolution,'x','split');
-                wid=str2double(R{1});
-                hei=str2double(R{2});
-                o.zoomboxidx=false(hei,wid,3);
-                leftx=max(1,min(o.zoombox(1),wid));
-                topy=max(1,min(o.zoombox(2),hei));
-                rightx=max(1,min(o.zoombox(1)+o.zoombox(3),wid));
-                bottomy=max(1,min(o.zoombox(2)+o.zoombox(4),wid));
-                o.zoomboxidx(topy:bottomy,leftx:rightx,:)=true;
-                o.zoomboxwid=rightx-leftx+1;
-                o.zoomboxhei=bottomy-topy+1;
-                if all(o.zoomboxidx)
-                    o.zoomboxidx=[]; % no need to zoom if entire frame is inside box
-                end
-            end
-        end
-        function afterFrame(o)
-            % Getting single snaphot from the webcam takes about half a ms.
-            % But ask them in rapid succession and it takes much longer.
-            % Presumably it gives the same snapshot only once, and then
-            % when asked for a snapshot again we have to wait for the
-            % webcam to make the next frame. Therefore, only ask for
-            % snapshot at a rate that's below the framerate of the
-            % camera.
-            persistent lastsnap
-            if isempty(lastsnap) || GetSecs-lastsnap>1/o.snaphz
-                [ss, o.timestamps]=o.cam.snapshot;
-                lastsnap=GetSecs;
-                if ~isempty(o.zoomboxidx)
-                    ss=reshape(ss(o.zoomboxidx),o.zoomboxhei,o.zoomboxwid,3);
-                end
-                if o.gray
-                    ss=rgb2gray(ss);
-                end
-                o.snaps=ss;
-            end
+            o=o@neurostim.plugin(c,name);
+            o.RC=udpcam_remote_control;
         end
         function afterExperiment(o)
-            o.cam.delete;
+            delete(o.RC)
         end
     end
 end
