7d0cbd535cda877014c8e4c0d317841555970ab5
ktechvision <ktech@vision.rutgers.edu>
2019-04-17 08:22:44 -0400

    Failed attempt at rewriting the callback code...
    It is not clear how this depends on the C-code - lots of untractable errors.

--- +neurostim/+plugins/eyelink.m
+++ +neurostim/+plugins/eyelink.m
@@ -75,11 +75,22 @@ classdef eyelink < neurostim.plugins.eyetracker
         getSamples@logical=true;
         getEvents@logical=false;
         nTransferAttempts = 5;
-    end
     
-    properties
         doTrackerSetup@logical  = true;  % Do it before the next trial
         doDriftCorrect@logical  = false;  % Do it before the next trial
+    
+      
+    end
+    
+    properties (SetAccess= public, GetAccess=public)
+        
+        % Callback props
+        inDisplayEye@logical =false;
+        inDriftCorrect@logical = false;
+        eyeImageTexture;
+          % Callback props
+        eyeImageSize  =30; % Percent of screen
+        
     end
     
     properties (Dependent)
@@ -130,9 +141,9 @@ classdef eyelink < neurostim.plugins.eyetracker
             end
             %Initialise connection to Eyelink.
             if ~o.useMouse
-                result = Eyelink('Initialize', 'PsychEyelinkDispatchCallback');
+                result = Eyelink('Initialize', 'neurostimEyelinkDispatchCallback');
             else
-                result = Eyelink('InitializeDummy', 'PsychEyelinkDispatchCallback');
+                result = Eyelink('InitializeDummy', 'neurostimEyelinkDispatchCallback');
                 %result =0;
             end
             
@@ -143,8 +154,6 @@ classdef eyelink < neurostim.plugins.eyetracker
             
             o.el.TERMINATE_KEY = o.el.ESC_KEY;  % quit using ESC
             
-            % Tell eyelink about the o.el properties we just set.
-            PsychEyelinkDispatchCallback(o.el);
             
             %Tell Eyelink about the pixel coordinates
             Eyelink('Command', 'screen_pixel_coords = %d %d %d %d',o.cic.screen.xorigin,o.cic.screen.yorigin,o.cic.screen.xorigin+o.cic.screen.xpixels,o.cic.screen.yorigin + o.cic.screen.ypixels);
@@ -218,10 +227,10 @@ classdef eyelink < neurostim.plugins.eyetracker
                 o.el.calibrationtargetwidth = o.clbTargetSize/2/o.cic.screen.width*100; %default to half radius
             else
                 o.el.calibrationtargetwidth = o.clbTargetInnerSize/o.cic.screen.width*100;
-            end
-            
-            o.el.callback = @o.dispatchCallback;
-            dispatchCallback(o,o.el);
+            end    
+            o.el.callback  ='neurostimEyelinkDispatchCallback';
+            o.el.o = o;
+            EyelinkUpdateDefaults(o.el);
         end
         
         function afterExperiment(o)
@@ -429,197 +438,7 @@ classdef eyelink < neurostim.plugins.eyetracker
         
         %% Dispatch
         
-        function rc = dispatchCallback(o,args, msg)
-            % Adapted from PsychEyelinkDispatchCallback - The host computer
-            % calls this with various arguments (callArgs) and those then get
-            % passed to plugin member functions for actual drawing to the
-            % screen.
-            
-            % TODO
-            % replace persistent with object vars
-            % Set this up directly without EyelinkInitDefaults
-            
-            % BK April 2019
-            
-            if nargin < 2
-                msg = [];                
-            end            
-            if numel(args)~=4
-                error('Incorrect arguments to the Eyelink callback');
-            end
-            eyeCmd = args(1);
-            
-            
-            
-            
-            
-            o.inEyeDisplay=0; % Seton setup call
-            
-            
-            
-            % Flag that tells if a new camera image was received and our camera image texture needs update:
-            newCamImage = 0;
-            needsUpdate = 1;
-            
-            switch eyeCmd
-                case 1
-                    % New videoframe received. See code below for actual processing.
-                    newCamImage = 1;
-                case 2
-                    % Eyelink Keyboard query:
-                    [rc, o.el] = EyelinkGetKey(o.el);
-                    needsUpdate = 0;
-                case 3
-                    % Alert message:
-                    o.writeToFeed(sprintf('Eyelink Alert: %s.\n', msg));
-                    needsUpdate = 0;
-                case 4
-                    % Image title of camera image transmitted from Eyelink:
-                    if args(2) ~= -1
-                        o.title = sprintf('Camera: %s [Threshold = %f]', msg, args(2));
-                    else
-                        o.title = msg;
-                    end
-                case 5
-                    % Define calibration target and enable its drawing:
-                    calXY = args(2:3);
-                    clearScreen=1;
-                case 6
-                    % Clear calibration display:
-                    clearScreen=1;
-                    drawInstructions=1;
-                case 7
-                    % Setup calibration display:
-                    if o.inDrift
-                        drawInstructions = 0;
-                        o.inDrift = false;
-                    else
-                        drawInstructions = 1;
-                    end
-                    clearScreen=1;
-                case 8
-                    newCamImage = 1;
-                    % Setup image display:
-                    o.inEyeDisplay=1;
-                    drawInstructions=1;
-                case 9
-                    % Exit image display:
-                    clearScreen=1;
-                    o.inEyeDisplay=0;
-                    drawInstructions=1;
-                case 10
-                    % Erase current calibration target:
-                    calXY = [];
-                    clearScreen=1;
-                case 11
-                    clearScreen=1;
-                case 12
-                    % New calibration target sound:
-                    makeSound(o,'cal_target_beep');
-                    needsUpdate = 0;
-                case 13
-                    % New drift correction target sound:
-                    makeSound(o, 'drift_correction_target_beep');
-                    needsUpdate = 0;
-                case 14
-                    % Calibration done sound:
-                    errc = args(2);
-                    if errc > 0
-                        % Calibration failed:
-                        makeSound(o, 'calibration_failed_beep');
-                    else
-                        % Calibration success:
-                        makeSound(o, 'calibration_success_beep');
-                    end
-                    needsUpdate = 0;
-                case 15
-                    % Drift correction done sound:
-                    errc = args(2);
-                    if errc > 0
-                        % Drift correction failed:
-                        makeSound(o, 'drift_correction_failed_beep');
-                    else
-                        % Drift correction success:
-                        makeSound(o, 'drift_correction_success_beep');
-                    end
-                    needsUpdate = 0;
-                case 16
-                    [width, height]=Screen('WindowSize', o.window);
-                    % get mouse
-                    [x,y, buttons] = GetMouse(o.window);
-                    
-                    HideCursor
-                    if find(buttons)
-                        rc = [width , height, x , y,  dw , dh , 1];
-                    else
-                        rc = [width , height, x , y , dw , dh , 0];
-                    end
-                    needsUpdate = 0;
-                case 17
-                    o.inDrift =1;
-                    needsUpdate = 0;
-                otherwise
-                    % Unknown command:
-                    o.writeToFeed(sprintf('Eyelink callback: Unknown eyelink command (%i)\n', eyeCmd));
-                    needsUpdate = 0;
-            end
-            
-            if ~needsUpdate
-                % Nope. Return from callback:
-                return;
-            end
-            
-            % Need to rebuild/redraw and flip the display:
-            % need to clear screen?
-            if clearScreen==1
-                Screen('FillRect', o.window, o.backgroundColor);
-            end
-            % New video data from eyelink?
-            if newCamImage
-                % Video callback from Eyelink: We have a 'eyewidth' by 'eyeheight' pixels
-                % live eye image from the Eyelink system. Each pixel is encoded as a 4 byte
-                % RGBA pixel with alpha channel set to a constant value of 255 and the RGB
-                % channels encoding a 1-Byte per channel R, G or B color value. The
-                % given 'eyeimgptr' is a specially encoded memory pointer to the memory
-                % buffer inside Eyelink() that encodes the image.
-                ptr = args(2);
-                imWidth  = args(3);
-                imHeight = args(4);
-                
-                % Create a new PTB texture of proper format and size and inject the 4
-                % channel RGBA color image from the Eyelink memory buffer into the texture.
-                % Return a standard PTB texture handle to it. If such a texture already
-                % exists from a previous invocation of this routiene, just recycle it for
-                % slightly higher efficiency:
-                
-                GL_RGBA = 6408;
-                GL_RGBA8 = 32856;
-                GL_UNSIGNED_INT_8_8_8_8_REV = 33639;
-                hostDataFormat = GL_UNSIGNED_INT_8_8_8_8_REV;
-                o.eyeImageTexture = Screen('SetOpenGLTextureFromMemPointer', o.window,  o.eyeImageTexture, ptr, imWidth, imHeight, 4, 0, [], GL_RGBA8, GL_RGBA, hostDataFormat);
-            end
-            
-            % If we're in imagemodedisplay, draw eye camera image texture centered in
-            % window, if any such texture exists, also draw title if it exists.
-            if ~isempty(eyeImageTexture) && o.inEyeDisplay==1
-                o.drawCameraImage;
-            end
-            
-            % Draw calibration target, if any is specified:
-            if ~isempty(calXY)
-                drawInstructions=0;
-                o.drawCalibrationTarget(calXY);
-            end
-            
-            % Need to draw instructions?
-            if drawInstructions==1
-                 o.cic.drawFormattedText(msg)
-            end
-            
-            Screen('Flip', o.window, [], 1, 1); %Immediate flip,no clear.
-            
-            
-        end
+        
         
         
         function  drawCameraImage(o)
@@ -628,7 +447,7 @@ classdef eyelink < neurostim.plugins.eyetracker
                     eyerect=Screen('Rect', o.eyeImageTexture);
                     wrect=Screen('Rect', o.window);
                     [width, ~]=Screen('WindowSize', o.window);
-                    dw=round(o.el.eyeimgsize/100*width);
+                    dw=round(o.eyeImageSize/100*width);
                     dh=round(dw * eyerect(4)/eyerect(3));
                     
                     drect=[ 0 0 dw dh ];
@@ -695,9 +514,11 @@ classdef eyelink < neurostim.plugins.eyetracker
         
         
         
-        function drawCalibrationTarget(o,calxy)
-            outerRect = [];
-            innerRect = [];
+        function drawCalibrationTarget(o,xy)
+            outerRect =o.clbTargetSize *[-0.5 0.5 0.5 -0.5];
+            outerRect = CenterRectOnPointd(outerRect,xy(1),xy(2));
+            innerRect = o.clbTargetInnerSize *[-0.5 0.5 0.5 -0.5];
+            innerRect = CenterRectOnPointd(innerRect,xy(1),xy(2));
             Screen('FillOval', o.window, o.clbTargetColor, outerRect);
             Screen('FillOval', o.window, o.backgroundColor, innerRect);
         end
@@ -705,4 +526,7 @@ classdef eyelink < neurostim.plugins.eyetracker
         
     end
     
+    methods (Static)
+     
+    end
 end
