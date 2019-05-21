02df16ae92d8a29080be0ae8123b635c6c1827d6
Shaun Cloherty <s.cloherty@ieee.org>
2019-05-21 17:56:17 +1000

    Updated daqDemo.m

--- demos/daqDemo.m
+++ demos/daqDemo.m
@@ -1,5 +1,18 @@
-function vpixxDemo(varargin)
-% Demonstrate use of VPixx daq plugin.
+function daqDemo(varargin)
+% Demonstrate use of the mcc daq plugin.
+%
+% Optional arguments:
+%
+%   'bit' - digital output bit (1-8) to be toggled (Default: 1)
+
+% 2019-05-21 - Shaun L. Cloherty <s.cloherty@ieee.org>
+
+p = inputParser;
+p.KeepUnmatched = true;
+p.addParameter('bit',1,@(x) validateattributes(x,{'numeric'},{'nonempty','scalar','>=',1,'<=',8}));
+
+p.parse(varargin{:});
+args = p.Results;
 
 import neurostim.*
 commandwindow;
@@ -13,7 +26,7 @@ m =  plugins.mcc(c);
 % add stimuli
 
 % fixation target
-f=stimuli.fixation(c,'fix');
+f = stimuli.fixation(c,'fix');
 f.shape = 'CIRC';
 f.size = 0.25;
 f.color = [1 0 0]; % red
@@ -22,8 +35,6 @@ f.duration = Inf;
 
 % random dot pattern
 d = stimuli.rdp(c,'dots');
-d.X = '@fix.X';                 
-d.Y = '@fix.Y';                 
 d.on = plugins.jitter(c,{250,500},'distribution','uniform'); % Turn on at random times
 d.duration = 1000;
 d.color = [1 1 1]; % white
@@ -33,22 +44,20 @@ d.maxRadius = 5;
 d.lifetime = Inf;
 d.noiseMode = 1;
 
-% add on onsetFcn to set DAQ bit 9 on the mcc HIGH when the dots appear
-d.onsetFunction = @(o,t) o.cic.mcc.digitalOut(8+1,true);
+% add on onsetFcn to set DAQ bit HIGH when the dots appear
+d.onsetFunction = @(o,t) o.cic.mcc.digitalOut(8+args.bit,true);
 
-% add on offsetFcn to clear DAQ bit 9 on the mcc when the dots disappear
-d.offsetFunction = @(o,t) o.cic.mcc.digitalOut(8+1,false);
+% add on offsetFcn to clear DAQ bit when the dots disappear
+d.offsetFunction = @(o,t) o.cic.mcc.digitalOut(8+args.bit,false);
 
 % experiment design
-c.trialDuration = 2000;
-% c.iti = 2000;
+c.trialDuration = '@dots.duration + 2*dots.on';
 
 % specify experimental conditions
 fac = design('myFactorial');
-fac.fac1.fix.X = {-10, 0, 10}; % three positions
-fac.fac2.dots.direction = {-90, 90}; % two dot directions
+fac.fac1.dots.direction = {-90, 90}; % upward or downward motion
 
-fac.conditions(:,:).dots.duration = plugins.jitter(c,{500,1500},'distribution','uniform');
+fac.conditions(:).dots.duration = plugins.jitter(c,{500,1000},'distribution','uniform');
 
 % specify a block of trials
 blk = block('myBlock',fac);
