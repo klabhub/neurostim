klabhub/neurostim

commit 1988ab51f5fa5136928c39a4d13480c54583dd53

Fix updating of adaptive plugins (c.f. #190)

+neurostim/cic.m
@@ -952,6 +952,29 @@ classdef cic < neurostim.plugin
             %% Set up order and blocks
             order(c,c.pluginOrder);
             setupExperiment(c,block1,varargin{:});
+
+            % Force adaptive plugins assigned directly to parameters into
+            % the block design object(s) instead. This ensures the adaptive
+            % plugins are updated correctly (by the block object(s)).
+            plgs = c.order; % *all* plugins
+            for ii = 1:numel(plgs)
+              plg = plgs{ii}; 
+              prms = prmsByClass(c.(plg),'neurostim.plugins.adaptive');
+              if isempty(prms)
+                % no adaptive plugins/parameters
+                continue
+              end
+
+              for jj = 1:numel(prms)
+                prm = prms{jj};
+                obj = c.(plg).(prm);
+                c.(plg).(prm) = obj.getAdaptValue(); % default value?
+ 
+                % loop over blocks, adding plg.prm = obj
+                arrayfun(@(x) addAdaptive(x,plg,prm,obj),c.blocks);
+              end
+            end
+
             %%Setup PTB imaging pipeline and keyboard handling
             PsychImaging(c);
             checkFrameRate(c);

+neurostim/block.m
@@ -253,6 +253,20 @@ classdef block < dynamicprops
             waitForKey = o.beforeKeyPress && (~isempty(msg) || ~isempty(o.beforeFunction));
         end
         
+        function addAdaptive(o,plg,prm,obj)
+          % Add plg.prm = obj to each design
+          %
+          % Usage:
+          %
+          %   addAdaptive(o,plg,prm,obj) 
+          %
+          % where
+          %
+          %   plg - the name of a plugin
+          %   prm - the name of a parameter of plg
+          %   obj - an adaptive plugin object
+          arrayfun(@(x) addAdaptive(x,plg,prm,obj),o.designs);
+        end
     end % methods
     
 end % classdef

+neurostim/design.m
@@ -402,6 +402,34 @@ classdef design <handle & matlab.mixin.Copyable
             o.currentTrialIx =1; % Reset the index to start at the first entry
         end
         
+        function addAdaptive(o,plg,prm,obj)
+          % If plg.prm is NOT part of the design, plg.prm = obj is added to each condition
+          % 
+          % Usage:
+          %
+          %   addAdaptive(o,plg,prm,obj)
+          %
+          % where
+          %
+          %   plg - the name of a plugin
+          %   prm - the name of a parameter of plg
+          %   obj - an adaptive plugin object
+          assert(isa(obj,'neurostim.plugins.adaptive'),'obj must be an adaptive plugin');
+
+          % if the specified plg.prm is not already part of the design,
+          % then we add plg.prm = obj to all conditions 
+          specs = cat(1,o.factorSpecs{:},o.conditionSpecs{:});
+
+          if any(strcmpi(specs(:,1),plg) & strcmpi(specs(:,2),prm))
+            % plg.prm is already part of the design... DO NOT overwrite it
+            return
+          end
+
+          % call subsasgn to add plg.prm = obj to all conditions... as if
+          % the user specified o.conditions(:).plg.prm = obj
+          o = subsasgn(o,struct('type',{'.','()','.','.'},'subs',{'conditions',{':'},plg,prm}),obj); %#ok<NASGU> 
+        end
+
         function o = subsasgn(o,S,V)
             % subsasgn to create special handling of .weights .conditions, and
             % .facN design specifications.

+neurostim/plugin.m
@@ -176,6 +176,13 @@ classdef plugin  < dynamicprops & matlab.mixin.Copyable & matlab.mixin.Heterogen
             o.prms.(parm.name) = duplicate(parm,o,h);
         end
         
+        function prms = prmsByClass(o,cls)
+          % return the names of the ns parameters that are
+          % instances of the class specified by cls
+          prms = fieldnames(o.prms);
+          ix = cellfun(@(x) isa(o.(x),cls),prms);
+          prms(~ix) = [];
+        end
         
         
         function tbl = getBIDSTable(o,varargin)

demos/adaptiveDemo.m
@@ -1,4 +1,4 @@
-function adaptiveDemo
+function adaptiveDemo(varargin)
 % Demo to show adaptive threshold estimation.
 %
 % The subjects task is to detect the location of a Gabor:  left (press
@@ -26,7 +26,8 @@ simulatedObserver = '@(grating.contrast<(0.1+0.5*(cic.condition-1)))+1.0';
 % Or use this one for an observer with some zero mean gaussian noise on the threshold
 %simulatedObserver = '@(grating.contrast< (0.5*randn + (0.1+0.5*(cic.condition-1))))+1.0';
 %% Setup the controller 
-c= myRig;
+c= myRig(varargin{:});
+
 c.trialDuration = Inf;
 c.screen.color.background = [ 0.5 0.5 0.5];
 c.subjectNr= 0;

commit 6e9da3601d753652d9cd542c65d3f240ba82501d

Fix a plugin (re-)ordering bug (re: #200).
This change fixes a bug introduced by commit 1988ab5 (re: #190)
that causes any changes to the default plugin order, imposed in
cic.run(), to be immediately reverted when handling adaptive
plugins/parameters (see cic.handleAdaptives()).

This change removes the call to c.order(), which reverts the plugin
order, in favour of using c.pluginOrder directly.

+neurostim/cic.m
@@ -1790,9 +1790,9 @@ classdef cic < neurostim.plugin
             % the block design object(s) instead. This ensures the adaptive
             % plugins are updated correctly (by the block object(s)).
            
-            plgs = c.order; % *all* plugins
+            plgs = {c.pluginOrder.name}; % *all* plugins
             for ii = 1:numel(plgs)
-              plg = plgs{ii}; 
+              plg = plgs{ii};
               prms = prmsByClass(c.(plg),'neurostim.plugins.adaptive');
               if isempty(prms)
                 % no adaptive plugins/parameters