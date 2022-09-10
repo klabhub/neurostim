a98d00421d9e7553807e052bb47e8a4aa29d2423
Daisuke <sageagainstthemachine@gmail.com>
2022-09-10 23:07:44 +1000

    ITI jitter demo
    added some description about the demo code

--- demos/jitteredITIDemo.m
+++ demos/jitteredITIDemo.m
@@ -1,9 +1,13 @@
 function jitteredITIDemo(minJitter, maxJitter, varargin)
 % Demo to show variable inter-trial interval.
 %
+% INPUT:
+% minJitter: minimum inter-trial interval in [s]
+% maxJitter: maximum inter-trial interval in [s]
+%
 % cf. https://github.com/klabhub/neurostim/issues/204
 %
-% DS  - Aug 2022.
+% DS  - Sep 2022.
 
 import neurostim.*
 
@@ -15,16 +19,11 @@ if nargin < 1
 end
 
 %% Setup the controller 
-c= myRig(varargin{:});
-
-%c.trialDuration = inf;
+c = myRig(varargin{:});
 c.screen.color.background = [ 0.5 0.5 0.5];
 c.subjectNr= 0;
 
 %% Add a Gabor;
-% We'll simulate an experiment in which
-% the grating's location (left or right) is to be detected
-% and use this to estimate the contrast threshold
 g=stimuli.gabor(c,'grating');
 g.color            = [0.5 0.5 0.5];
 g.contrast         = 1;
@@ -37,6 +36,13 @@ g.mask             = 'CIRCLE';
 g.frequency        = 3;
 g.on               =  0;
 g.duration         = 200;
+
+
+%% key part - adding the property to a regular plugin (gabor in this case), not to CIC
+% Properties of cic are special (in the sense that they are not updated at 
+% the start of the trial ) and can therefore not be set in a design. This 
+% is to avoid all kinds of issues with Internal /bookkeeping parameters of 
+% CIC being changed on a per-trial basis.
 
 g.addProperty('jitteredITI',[]);
 g.jitteredITI = plugins.jitter(c,{minJitter, maxJitter}); 
